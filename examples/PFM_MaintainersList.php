715a57388267795d6a714ce9e96164c4de4d4954
Laurent Laville <farell@php.net>
2006-05-13 13:14:08 +0000

    initial commit example for api 0.3.0 ; will change after config file management rewrite (TODO)
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager_Frontend/trunk@212941 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/PFM_MaintainersList.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * An example script that retrieves maintainers list
+ * from PEAR::PEAR_PackageFileManager package
+ * using PEAR_PackageFileManager_Frontend with no front end.
+ *
+ * @author    Laurent Laville
+ * @package   PEAR_PackageFileManager_Frontend
+ * @version   $Id$
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @copyright 2006 Laurent Laville
+ * @ignore
+ */
+
+require_once 'PEAR/PackageFileManager/Frontend.php';
+
+/**
+ * This class allow to use PEAR_PackageFileManager_Frontend as backend without any frontend.
+ * No end-user action needed.
+ * @ignore
+ */
+class PEAR_PackageFileManager_Frontend_Null extends PEAR_PackageFileManager_Frontend
+{
+
+    function PEAR_PackageFileManager_Frontend_Null($driver, $packagedirectory, $pathtopackagefile)
+    {
+        parent::PEAR_PackageFileManager_Frontend($driver, $packagedirectory, $pathtopackagefile);
+    }
+}
+
+/**
+ * A wrapper for PEAR::Var_Dump package or default php var_dump() function.
+ * @ignore
+ */
+function varDump($var)
+{
+    $available = PEAR_PackageFileManager2::isIncludeable('Var_Dump.php');
+    if ($available) {
+        include_once 'Var_Dump.php';
+        Var_Dump::display($var, false, array('display_mode' => 'HTML4_Table'));
+    } else {
+        echo '<pre style="background-color:#eee; color:#000; padding:1em;">';
+        var_dump($var);
+        echo '</pre>';
+    }
+}
+
+/**
+ * Callback error handler for PEAR_PackageFileManager_Frontend and
+ * its error stack.
+ * @ignore
+ */
+function haltOnError($err)
+{
+    if ($err['level'] == 'error') {
+        echo __FUNCTION__;
+        varDump($err);
+        return PEAR_ERRORSTACK_DIE;
+    }
+}
+
+session_start();
+
+// configuration options
+$conf['pfm']['baseinstalldir'] = 'PEAR';
+$config = array($conf, 'phparray');
+
+
+// where to find package sources
+$pkgDir = 'd:/php/pear/PEAR_PackageFileManager';
+
+PEAR_ErrorStack::staticPushCallback('haltOnError');
+
+$pfmfe =& PEAR_PackageFileManager_Frontend::singleton('Null', $pkgDir);
+$pfmfe->loadPreferences($config);
+
+echo '<h1>All maintainers</h1>';
+$maint = $pfmfe->getMaintList();
+varDump($maint);
+
+echo '<h1>Leader only </h1>';
+$maint = $pfmfe->getMaintList('lead');
+varDump($maint);
+
+echo '<h1>Developper only </h1>';
+$maint = $pfmfe->getMaintList('developer');
+varDump($maint);
+
+echo '<h1>unknown users </h1>';
+$maint = $pfmfe->getMaintList('users');
+varDump($maint);
+
+?>
---
+++ examples/THL_Infos.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * An example script that retrieves informations
+ * from PEAR::Text_Highlighter package
+ * using PEAR_PackageFileManager_Frontend with no front end.
+ *
+ * @author    Laurent Laville
+ * @package   PEAR_PackageFileManager_Frontend
+ * @version   $Id$
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @copyright 2006 Laurent Laville
+ * @ignore
+ */
+
+require_once 'PEAR/PackageFileManager/Frontend.php';
+
+/**
+ * This class allow to use PEAR_PackageFileManager_Frontend as backend without any frontend.
+ * No end-user action needed.
+ * @ignore
+ */
+class PEAR_PackageFileManager_Frontend_Null extends PEAR_PackageFileManager_Frontend
+{
+
+    function PEAR_PackageFileManager_Frontend_Null($driver, $packagedirectory, $pathtopackagefile)
+    {
+        parent::PEAR_PackageFileManager_Frontend($driver, $packagedirectory, $pathtopackagefile);
+    }
+}
+
+/**
+ * A wrapper for PEAR::Var_Dump package or default php var_dump() function.
+ * @ignore
+ */
+function varDump($var)
+{
+    $available = PEAR_PackageFileManager2::isIncludeable('Var_Dump.php');
+    if ($available) {
+        include_once 'Var_Dump.php';
+        Var_Dump::display($var, false, array('display_mode' => 'HTML4_Table'));
+    } else {
+        echo '<pre style="background-color:#eee; color:#000; padding:1em;">';
+        var_dump($var);
+        echo '</pre>';
+    }
+}
+
+session_start();
+
+// configuration options
+$conf['pfm']['baseinstalldir'] = 'Text';
+$config = array($conf, 'phparray');
+
+// where to find package sources
+$pkgDir = 'E:/PEAR/Text/Text_Highlighter-0.6.9';
+
+$pfmfe =& PEAR_PackageFileManager_Frontend::singleton('Null', $pkgDir);
+$pfmfe->loadPreferences($config);
+if ($pfmfe->hasErrors()) {
+    $errors = $pfmfe->getErrors();
+    varDump($errors);
+    die('exit on Error');
+}
+
+echo '<h1>Package Summary </h1>';
+$def = $pfmfe->getDefaults('package');
+varDump($def);
+
+echo '<h1>Latest Release </h1>';
+$def = $pfmfe->getDefaults('release');
+varDump($def);
+
+echo '<h1>Maintainers </h1>';
+$def = $pfmfe->getDefaults('maintainers');
+varDump($def);
+
+echo '<h1>Dependencies </h1>';
+$def = $pfmfe->getDefaults('dependencies');
+varDump($def);
+?>
